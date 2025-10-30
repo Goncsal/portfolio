edc9d5e9835795c01e879fec4e5bacbca9fe98f9
Goncsal <goncalodfalmeida@ua.pt>
2025-10-30 10:18:00 +0000

    added base path for github pages

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import tailwindcss from '@tailwindcss/vite'
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   assetsInclude: ['**/*.md'],
+  base: '/portfolio&writeUps/',
   build: {
     rollupOptions: {
       output: {
